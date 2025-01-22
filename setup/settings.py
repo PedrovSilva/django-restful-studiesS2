8c731fb98a1cfbd84585799229ea15eb749f1a81
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2025-01-22 20:36:31 +0000

    csrf fix

--- setup/settings.py
+++ setup/settings.py
@@ -112,7 +112,7 @@ TIME_ZONE = "America/Manaus"
 USE_I18N = True
 
 USE_TZ = True
-
+CSRF_TRUSTED_ORIGINS = ['https://localhost:8000']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
