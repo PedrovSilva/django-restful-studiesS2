f2457c3a9ee707da066e623c1dbea838a59aab25
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2025-01-22 21:06:29 +0000

    create viewSets

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,11 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializer import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets
 
-def estudantes(request):
-    if request.method == "GET":
-        estudante = {
-            "id":1,
-            "nome": "Shrek"
-        }
-        return JsonResponse(estudante)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
