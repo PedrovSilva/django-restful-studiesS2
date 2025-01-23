b97a6afdbaa4293430f6f97146ba738b71df9ab6
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2025-01-23 19:23:26 +0000

    auth

--- escola/views.py
+++ escola/views.py
@@ -2,25 +2,31 @@ from escola.models import Estudante, Curso, Matricula
 from escola.serializer import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatricularEstudanteSerializer, ListaMatriculasCursoSerializer
 from rest_framework import viewsets, generics
 
+
 class EstudanteViewSet(viewsets.ModelViewSet):
+    
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
+  
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
+    
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 class ListaMatriculaEstudante(generics.ListAPIView):
+   
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs["pk"])
         return queryset
     serializer_class = ListaMatricularEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
+ 
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs["pk"])
         return queryset
